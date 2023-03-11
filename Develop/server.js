3d3262e86a1a2151e1d77bb57e056a51b0feb3cd
WalleyemanZ <jacobzea96@gmail.com>
2023-03-11 12:51:31 -0700

    added dependencies, middleware, and some routes to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,4 @@
+// PSUDOCODE
 // bring in dependencies
 // configure middleware
 // Routes
@@ -13,4 +14,29 @@
 
         // app.get('/api/notes')
 
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded ({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sentFile(path.json(__dirname, '/public/notes.html'))
+}); 
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
 
