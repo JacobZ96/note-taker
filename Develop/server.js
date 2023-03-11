d9082d02e018d1038d3d3548bcfa05424121fcb6
WalleyemanZ <jacobzea96@gmail.com>
2023-03-10 19:00:01 -0700

    copied started code and added psudocode to server.js

---
+++ Develop/server.js
@@ -0,0 +1,16 @@
+// bring in dependencies
+// configure middleware
+// Routes
+    // homepage 
+        // eg app.get('/')
+        // send back the index.html
+    // notes 
+        // eg app.get('/notes')
+    //notes api    
+        // app.delete('/api/notes/:noteId')
+
+        // app.post('/api/notes')
+
+        // app.get('/api/notes')
+
+
